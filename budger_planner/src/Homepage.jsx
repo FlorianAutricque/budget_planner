4f03b18af680e8dd0f6547f568cb42b63c1f7f2f
florian autricque <fautricque@gmail.com>
2024-08-09 17:06:30 -0400

    homepage

--- budger_planner/src/Homepage.jsx
+++ budger_planner/src/Homepage.jsx
@@ -1,5 +1,35 @@
+import { NavLink } from "react-router-dom";
+
 function Homepage() {
-  return <div>homepage</div>;
+  const months = [
+    { id: 1, nameMonth: "Januray" },
+    { id: 2, nameMonth: "February" },
+    { id: 3, nameMonth: "March" },
+    { id: 4, nameMonth: "April" },
+    { id: 5, nameMonth: "May" },
+    { id: 6, nameMonth: "June" },
+    { id: 7, nameMonth: "July" },
+    { id: 8, nameMonth: "August" },
+    { id: 9, nameMonth: "September" },
+    { id: 10, nameMonth: "October" },
+    { id: 11, nameMonth: "November" },
+    { id: 12, nameMonth: "December" },
+  ];
+
+  const day = new Date();
+  const currentMonthIndex = day.getMonth();
+  const month = months[currentMonthIndex];
+  const name = month ? month.nameMonth : "Unknown Month";
+
+  return (
+    <div>
+      <h2>Welcome to your budget planner</h2>
+
+      <p>We are in {name}, let&apos;s track our budget</p>
+
+      <NavLink to={`/month/${month.id}`}>Start now</NavLink>
+    </div>
+  );
 }
 
 export default Homepage;
